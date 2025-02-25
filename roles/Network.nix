b72a4b30fd014a950e3c8bee830b03eb5e6919fc
Yoran9111 <Yoranvuranok@gmail.com>
2025-02-25 13:47:27 +0100

    Update Network.nix

--- roles/Network.nix
+++ roles/Network.nix
@@ -3,11 +3,11 @@
 {
 networking.interfaces.eth0.ipv4.addresses = [
   {
-    address = "192.168.0.101";
+    address = "10.0.2.15";
     prefixLength = 24;  # Use an integer here, not a string
   }
 ];
 
-networking.defaultGateway = "192.168.0.251";
+networking.defaultGateway = "10.0.2.2";
 networking.nameservers = [ "8.8.8.8" ];
 }
