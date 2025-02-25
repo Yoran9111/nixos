bec0f91b58cff5d6bf724c502f7ba1a968dc2df1
Yoran9111 <Yoranvuranok@gmail.com>
2025-02-25 13:06:13 +0100

    Update Network.nix

--- roles/Network.nix
+++ roles/Network.nix
@@ -3,7 +3,7 @@
 {
 
   # Network configuration for a static IP
-  networking.interfaces.eth0 = {
+  networking.interfaces.enp0s3 = {
     ipv4.addresses = [
       {
         address = "192.168.1.100";  # Replace with your desired IP address
